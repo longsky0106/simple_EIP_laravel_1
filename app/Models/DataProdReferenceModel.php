486d2dbb70b9e8bcbc0c4b90cba9bdc71af0e336
posi.shen <woodmanany@gmail.com>
2023-01-06 09:07:01 +0800

    指定資料表名稱

--- app/Models/DataProdReferenceModel.php
+++ app/Models/DataProdReferenceModel.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class DataProdReferenceModel extends Model
 {
-    use HasFactory;
+    protected $connection = 'mysql2';
+    protected $table = 'Data_Prod_Reference';
+    protected $fillable =   [
+                                'Model',
+                                'SK_NO1'
+                            ];
+    //$timestamps = false;
 }
