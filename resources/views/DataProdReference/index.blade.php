13cbe40d3d980684096a8d5e5fe731621f79b1bd
posi.shen <woodmanany@gmail.com>
2023-01-13 16:17:15 +0800

    調整CSS

--- resources/views/DataProdReference/index.blade.php
+++ resources/views/DataProdReference/index.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/css/app.css')
+        
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -13,8 +13,10 @@
         <style>
             /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--tw-bg-opacity: 1;background-color:rgb(255 255 255 / var(--tw-bg-opacity))}.bg-gray-100{--tw-bg-opacity: 1;background-color:rgb(243 244 246 / var(--tw-bg-opacity))}.border-gray-200{--tw-border-opacity: 1;border-color:rgb(229 231 235 / var(--tw-border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{--tw-shadow: 0 1px 3px 0 rgb(0 0 0 / .1), 0 1px 2px -1px rgb(0 0 0 / .1);--tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color), 0 1px 2px -1px var(--tw-shadow-color);box-shadow:var(--tw-ring-offset-shadow, 0 0 #0000),var(--tw-ring-shadow, 0 0 #0000),var(--tw-shadow)}.text-center{text-align:center}.text-gray-200{--tw-text-opacity: 1;color:rgb(229 231 235 / var(--tw-text-opacity))}.text-gray-300{--tw-text-opacity: 1;color:rgb(209 213 219 / var(--tw-text-opacity))}.text-gray-400{--tw-text-opacity: 1;color:rgb(156 163 175 / var(--tw-text-opacity))}.text-gray-500{--tw-text-opacity: 1;color:rgb(107 114 128 / var(--tw-text-opacity))}.text-gray-600{--tw-text-opacity: 1;color:rgb(75 85 99 / var(--tw-text-opacity))}.text-gray-700{--tw-text-opacity: 1;color:rgb(55 65 81 / var(--tw-text-opacity))}.text-gray-900{--tw-text-opacity: 1;color:rgb(17 24 39 / var(--tw-text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--tw-bg-opacity: 1;background-color:rgb(31 41 55 / var(--tw-bg-opacity))}.dark\:bg-gray-900{--tw-bg-opacity: 1;background-color:rgb(17 24 39 / var(--tw-bg-opacity))}.dark\:border-gray-700{--tw-border-opacity: 1;border-color:rgb(55 65 81 / var(--tw-border-opacity))}.dark\:text-white{--tw-text-opacity: 1;color:rgb(255 255 255 / var(--tw-text-opacity))}.dark\:text-gray-400{--tw-text-opacity: 1;color:rgb(156 163 175 / var(--tw-text-opacity))}.dark\:text-gray-500{--tw-text-opacity: 1;color:rgb(107 114 128 / var(--tw-text-opacity))}}
         </style>
-
-        <style>
+		
+		@vite('resources/css/app.css')
+        
+		<style>
             body {
                 font-family: 'Nunito', sans-serif;
             }
@@ -25,20 +27,30 @@
     </head>
     <body>
         <div>
-            <div class="flex"><!-- pro_con_L: 欄，dr: 列 -->
+            <div class="flex">
 				<div class="flex">
-				    <div class="flex-1 w-9" >編號</div>
-				    <div class="flex-1 w-9">選擇</div>
+				    <div class="flex-3 min-w-[2em] border-[1px] 
+								border-red-400 bg-red-900 
+								text-white font-bold"
+					>
+						編號
+					</div>
+				    <div class="flex-3 min-w-[2em] border-[1px] 
+								border-red-400 bg-red-900 
+								text-white font-bold"
+					>
+						選擇
+					</div>
 				</div>
 				<div class="flex">
-                    <div class="flex-auto w-20" >圖片</div>
-                    <div class="flex-auto w-24" >型號 / 料號</div>
-                    <div class="flex-auto w-40" >產品分類</div>
-                    <div class="flex-auto w-32" >品名</div>
-                    <div class="flex-auto w-20" >官網頁面</div>
-                    <div class="flex-auto w-24" >售價與成本</div>
-                    <div class="flex-auto w-10" >庫存</div>
-                    <div class="flex-auto w-52" >銷售頁面範本</div>
+                    <div class="flex-auto w-20 border-2" >圖片</div>
+                    <div class="flex-auto w-24 border-2" >型號 / 料號</div>
+                    <div class="flex-auto w-52 border-2" >產品分類</div>
+                    <div class="flex-auto w-56 border-2" >品名</div>
+                    <div class="flex-auto w-20 border-2" >官網頁面</div>
+                    <div class="flex-auto w-24 border-2" >售價與成本</div>
+                    <div class="flex-auto w-10 border-2" >庫存</div>
+                    <div class="flex-auto w-52 border-2" >銷售頁面範本</div>
 				</div>
 			</div>
 
@@ -47,28 +59,28 @@
 
 			<div class="flex">
 				<div class="flex">
-					<div class="flex pn_L">
+					<div class="flex w-9 border-2">
 						 <div class="hidden" >編號</div>
 						 <div class="sk_data_L0 dr1_L" >{{ $count }}</div>
 					</div>
-					<div class="flex pn_L">
+					<div class="flex w-9 border-2">
 						<div class="hidden" >選擇</div>
 						<div class="sk_data_L1 dr1_L" id="list_no<?=$DataProdReference?>" ><input type="checkbox" name="Data_Prod_Ref_ID[]" value="{{ $DataProdReference->ID }}" autocomplete="off"></div>
 					</div>
 				</div>
 				<div class="flex">
-					<div class="pro_con_L2 pn_L">
+					<div class="w-20 border-2 pn_L">
 						<div class="hidden" >圖片</div>
 						<div class="sk_data_L2 dr1_L" > </div>
 					</div>
-					<div class="pro_con_L3 pn_L">
+					<div class="flex-auto w-24 border-2">
 						<div class="hidden" >型號/料號</div>
 						<div class="sk_data_L3 dr1_L" >
 							<div id=""><a href="javascript:prod_data_edit('{{ $DataProdReference->Model }}');">{{ $DataProdReference->Model }}</a></div>
 							<div>{{ $DataProdReference->SK_NO1 }}</div>
 						</div>
 					</div>
-					<div class="pro_con_L4 pn_L">
+					<div class="flex-auto w-52 border-2">
 						<div class="hidden" >分類</div>
 						<div class="sk_data_L4 dr1_L" >{{ $DataProdReference->SK_USE }} > {{ $DataProdReference->SK_LOCATE }}</div>
 					</div>
