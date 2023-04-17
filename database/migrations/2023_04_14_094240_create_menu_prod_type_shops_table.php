4dbba59313eaa19b63eda9f715353c89543cae58
posi.shen <woodmanany@gmail.com>
2023-04-17 09:30:13 +0800

    加入選單資料模型

---
+++ database/migrations/2023_04_14_094240_create_menu_prod_type_shops_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_prod_type_shops', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_prod_type_shops');
+    }
+};
