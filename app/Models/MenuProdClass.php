3eee9f202c34a698ba88186c19237aef97211791
posi.shen <woodmanany@gmail.com>
2023-04-25 14:52:11 +0800

    加上關聯註解

--- app/Models/MenuProdClass.php
+++ app/Models/MenuProdClass.php
@@ -13,7 +13,10 @@ class MenuProdClass extends Model
     protected $primaryKey = 'prod_class_id';
 
     public function MenuSpecItems() {
-        return $this->belongsToMany(MenuSpecItem::class, 'Menu_Spec_Link', 'MPC', 'MSI')
+        return $this->belongsToMany(MenuSpecItem::class, //對應目標
+                                    'Menu_Spec_Link', //聯合併的資料表名稱
+                                    'MPC', //關聯中的模型的外鍵名稱 (對到MenuProdClass的id)
+                                    'MSI') //合併的模型中的外鍵名稱 (對到MenuSpecItems的id)
         ->withPivot('MSI')
         ->using(TypeClassItemPivot::class);
     }    
