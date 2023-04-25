251d8ef6335103b315660f18f45b44c39cdd26f1
posi.shen <woodmanany@gmail.com>
2023-04-25 14:52:47 +0800

    修正value內容

--- resources/views/ProductDataManage/select-shopMenus2.blade.php
+++ resources/views/ProductDataManage/select-shopMenus2.blade.php
@@ -1,4 +1,4 @@
 <option value="0">選擇產品類別</option>
 @foreach($shopMenus2 as $key => $value)
-    <option value="{{ $shopMenus2[$key]['shop_menu2_id'] }}>">{{ $shopMenus2[$key]['shop_menu2_name'] }}</option>
+    <option value="{{ $shopMenus2[$key]['shop_menu2_id'] }}">{{ $shopMenus2[$key]['shop_menu2_name'] }}</option>
 @endforeach
