2f3cebd13aa4f7fed22d21383dc4fd25eff68f73
posi.shen <woodmanany@gmail.com>
2023-04-27 16:46:05 +0800

    銷售選單2

--- app/Models/MenuProdClassShop.php
+++ app/Models/MenuProdClassShop.php
@@ -10,5 +10,9 @@ class MenuProdClassShop extends Model
     // use HasFactory;
     protected $connection = 'sqlsrv';
     protected $table = 'Menu_Prod_Class_shop';
+    protected $primaryKey = 'shop_menu2_id';
 
+    public function MenuProdClass() {
+        return $this->hasOne(MenuProdClass::class,'prod_class_id', 'spec_menu_class_index'); //合併的模型中的外鍵名稱 (對到MenuSpecItems的id)
+    }
 }
