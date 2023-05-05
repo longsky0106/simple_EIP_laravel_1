be76c706259d4dbb10c26d91af8878a2eff4148f
posi.shen <woodmanany@gmail.com>
2023-05-05 09:41:31 +0800

    產品資料暫存資料表

---
+++ app/Models/SStockTemp.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SStockTemp extends Model
+{
+    protected $connection = 'sqlsr';
+    protected $table = 'SSTOCK_temp';
+    protected $primaryKey = 'SK_NO';
+    protected $fillable =   [
+        'SK_NO',
+        'SK_NAME',
+        'SK_SPEC',
+        'SK_COLOR',
+        'SK_SIZE',
+        'SK_USE',
+        'SK_LOCATE',
+        'SK_NOWQTY',
+        'SK_REM',
+        'SK_SESPES',
+        'SK_CSPES',
+        'SK_ESPES',
+        'SK_SMNETS'
+    ];
+}
