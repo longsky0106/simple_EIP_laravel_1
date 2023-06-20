6e09a6d3a76017fda9d529a18ff29f1121d64613
posi.shen <woodmanany@gmail.com>
2023-06-20 11:11:47 +0800

    補充欄位資料

--- app/Models/DataProdReferenceModel.php
+++ app/Models/DataProdReferenceModel.php
@@ -9,9 +9,33 @@ class DataProdReferenceModel extends Model
 {
     protected $connection = 'sqlsrv';
     protected $table = 'Data_Prod_Reference';
+    public $timestamps = false;
     protected $fillable =   [
                                 'Model',
-                                'SK_NO1'
+                                'SK_NO1',
+                                'SK_NO2',
+                                'SK_NO3',
+                                'SK_NO4',
+                                'HDMI',
+                                'DisplayPort',
+                                'DVI',
+                                'VGA',
+                                'USB-C(Data)',
+                                'USB-A',
+                                'RJ45',
+                                'SD Slot',
+                                'Micro SD Slot',
+                                'Audio',
+                                'Audio(TRRS)',
+                                'BC 1.2',
+                                'USB-C(5V/1.5A)',
+                                'PD',
+                                'Price',
+                                'Suggested Price',
+                                'Cost Price',
+                                'Main_Product',
+                                'Mark1',
+                                'Mark2'
                             ];
     //$timestamps = false;
 }
