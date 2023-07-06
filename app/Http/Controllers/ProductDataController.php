1ec30b8b8bad6603b978625fed403881d0bde850
posi.shen <woodmanany@gmail.com>
2023-07-06 10:29:32 +0800

    實作編輯與更新

--- app/Http/Controllers/ProductDataController.php
+++ app/Http/Controllers/ProductDataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\DataProdReferenceModel;
 use App\Models\MenuProdTypeShop;
 use Illuminate\Http\Request;
+use Throwable;
 
 class ProductDataController extends Controller
 {
@@ -192,7 +193,7 @@ class ProductDataController extends Controller
             }
 
             if(!empty($SK_NO4)){
-                echo "建立臨時料號...<br>";
+                echo "建立臨時料號並寫入規格資料...<br>";
                 app('App\Http\Controllers\SStockTempController')->store($request);                
             }
 
@@ -230,7 +231,99 @@ class ProductDataController extends Controller
      */
     public function edit($id)
     {
-        //
+
+      
+        
+        $shopMenus1 = MenuProdTypeShop::select('shop_menu1_id',
+                                                'shop_menu1_name',
+                                                'shop_menu1_rem')
+                                        ->get();
+        // return view('ProductDataManage.create', compact('shopMenus1', 'DefaultMenuSpecItems'));
+
+        $DataProdsReference = DataProdReferenceModel::select(
+            'ID'
+            , 'Model'
+            , DataProdReferenceModel::raw('(case when SSTOCK.SK_USE is NUll then SSTOCK_temp.SK_USE 
+                                            else SSTOCK.SK_USE 
+                                            end) as SK_USE')
+            , DataProdReferenceModel::raw('(case when SSTOCK.SK_LOCATE is NUll then SSTOCK_temp.SK_LOCATE 
+                                            else SSTOCK.SK_LOCATE 
+                                            end) as SK_LOCATE')
+            , DataProdReferenceModel::raw('(case when SSTOCKFD.fd_name is NUll then SSTOCKFD_temp.fd_name 
+                                            else SSTOCKFD.fd_name 
+                                            end) as fd_name')
+            , 'SSTOCK.SK_NAME'
+            ,'SK_NO1'
+            ,'SK_NO2'
+            ,'SK_NO3'
+            ,'SK_NO4'
+            ,'Price'
+            ,'Suggested Price'
+            ,'Cost Price'
+            ,'SPH_NowQtyByWare'
+        )->from(function ($sub) {
+            $sub->select('*')->from('Data_Prod_Reference');
+        }, 'PCT')
+        ->leftJoin('SSTOCK', 'SK_NO1', '=', DataProdReferenceModel::raw('SK_NO collate chinese_taiwan_stroke_ci_as'))
+        ->leftJoin('SSTOCK_temp', 'SK_NO4', '=', DataProdReferenceModel::raw('SSTOCK_temp.SK_NO collate chinese_taiwan_stroke_ci_as'))
+        ->leftJoin('SSTOCKFD', 'SK_NO1', '=', DataProdReferenceModel::raw('fd_skno collate chinese_taiwan_stroke_ci_as'))
+        ->leftJoin('SSTOCKFD_temp', 'SK_NO4', '=', DataProdReferenceModel::raw('SSTOCKFD_temp.fd_skno collate chinese_taiwan_stroke_ci_as'))
+        ->leftJoinSub(
+            DataProdReferenceModel::select('*')->from('View_SPHNowQtyByWare')->where('WD_WARE', '=', 'A'), 
+            'QTY', 'SK_NO1', '=', DataProdReferenceModel::raw('WD_SKNO collate chinese_taiwan_stroke_ci_as')
+        )
+        ->where('Model', $id)->first();
+
+        $SK_NO1 = $DataProdsReference->SK_NO1;
+        $SK_NO2 = $DataProdsReference->SK_NO2;
+        $SK_NO3 = $DataProdsReference->SK_NO3;
+        $SK_NO4 = $DataProdsReference->SK_NO4;
+
+        switch(true)
+        {
+            case !empty($SK_NO1):
+                $MainSK_NO = $SK_NO1;
+                break;
+            case !empty($SK_NO2):
+                $MainSK_NO = $SK_NO2;
+                break;
+            case !empty($SK_NO3):
+                $MainSK_NO = $SK_NO3;
+                break;
+            case !empty($SK_NO4):
+                $MainSK_NO = $SK_NO4;
+                break;
+            default:    
+            $MainSK_NO = '';
+        }
+
+        $DefaultMenuSpecItems = app('App\Http\Controllers\MenuSpecItemController')->show(0, $MainSK_NO);
+
+        if($MainSK_NO != $SK_NO4){
+            $DataSStock =  app('App\Http\Controllers\SStockController')->show($MainSK_NO);
+        }else{
+            $DataSStock =  app('App\Http\Controllers\SStockTempController')->show($MainSK_NO);
+        }
+
+        $description_all_tw = explode("---DESCRIPTION---", $DataSStock->SK_SMNETS)[0];
+        $zh_tw_description = rtrim(explode("---Features---", $description_all_tw)[0]);
+        
+        if(isset(explode("---DESCRIPTION---", $DataSStock->SK_SMNETS)[1])){
+            $description_all_en = explode("---DESCRIPTION---", $DataSStock->SK_SMNETS)[1];
+            $en_us_description = rtrim(explode("---Features---", $description_all_en)[0]);
+
+            $zh_tw_features = rtrim(explode("---Features---", $description_all_tw)[1]);
+            $en_us_features = rtrim(explode("---Features---", $description_all_en)[1]);            
+        }else{
+            $en_us_description = '';
+            $zh_tw_features = '';
+            $en_us_features = '';
+        }
+
+
+        return view('ProductDataManage.edit', compact('shopMenus1', 'DefaultMenuSpecItems', 'DataProdsReference', 
+                    'DataSStock', 'zh_tw_description', 'en_us_description', 'zh_tw_features', 'en_us_features'));
+
     }
 
     /**
@@ -242,7 +335,84 @@ class ProductDataController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $Sec = 3;
+        $error = 0;
+        $input = $request->input();
+        $SK_NO1 = $input['SK_NO1'];
+        $SK_NO2 = $input['SK_NO2'];
+        $SK_NO3 = $input['SK_NO3'];
+        $SK_NO4 = $input['SK_NO4'];
+        $categories = !empty($input['categories'])?($input['categories']):"0";
+        $ProdType = !empty($input['ProdType'])?($input['ProdType']):"0";
+        $name_for_sell_tw = $input['name_for_sell_tw'];
+        
+        if(empty($SK_NO1.$SK_NO2.$SK_NO3.$SK_NO4)){
+            echo "請至少填寫一個料號！<br>";
+            $error++;
+        }
+
+        if($categories+$ProdType==0){
+            echo "請設定產品分類！<br>";
+            $error++;
+        }
+
+        if($error>0){
+            return;
+        }else{
+            $SK_NO1 = empty($SK_NO1)?'':$SK_NO1;
+            $SK_NO2 = empty($SK_NO2)?'':$SK_NO2;
+            $SK_NO3 = empty($SK_NO3)?'':$SK_NO3;
+            $SK_NO4 = empty($SK_NO4)?'':$SK_NO4;
+
+            
+            echo "更新基本資料...<br>";dd($input);
+            try {
+                DataProdReferenceModel::find($id)->update(array_merge($input,
+                                                            [
+                                                                'SK_NO1' => $SK_NO1,
+                                                                'SK_NO2' => $SK_NO2,
+                                                                'SK_NO3' => $SK_NO3,
+                                                                'SK_NO4' => $SK_NO4
+                                                            ]));
+
+            } catch (Throwable $e) {
+                print "Error: ".$e->getMessage();
+                return ;
+            }
+
+            if(!empty($name_for_sell_tw)){
+                echo "儲存產品銷售用名稱...<br>";
+                $id = $input['create_Model'];
+                $fd_name = app('App\Http\Controllers\SStockFDTempController')->show($id);
+                if($fd_name->count() > 0){
+                    app('App\Http\Controllers\SStockFDTempController')->update($request, $id);
+                }else{
+                    app('App\Http\Controllers\SStockFDTempController')->store($request);
+                }
+                
+            }
+
+            for($i=1;$i<3;$i++) {
+                $item = "SK_NO".$i;
+                if(!empty($$item)){
+                    $id = $$item;
+                    echo "更新規格資料到料號".$id."...<br>";
+                    // app('App\Http\Controllers\SStockController')->update($request, $id);                
+                }
+            }
+
+            if(!empty($SK_NO4)){
+                echo "建立臨時料號並寫入規格資料...<br>";
+                app('App\Http\Controllers\SStockTempController')->store($request);                
+            }
+
+            echo $Sec.'秒後回到上一頁';
+/*             echo "<script>
+                    setTimeout(function(){ 
+                        window.location.href = '/ProductDataManage'; 
+                    }, ".($Sec*1000).");
+                  </script>"; */
+        }
     }
 
     /**
