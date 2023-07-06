781ee4e3f5849e6dea637dfdbf9b2bec66f30030
posi.shen <woodmanany@gmail.com>
2023-07-06 10:31:07 +0800

    增加資料欄位與修正

--- app/Http/Controllers/SStockController.php
+++ app/Http/Controllers/SStockController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\SStock;
+use Throwable;
 
 class SStockController extends Controller
 {
@@ -46,8 +47,20 @@ class SStockController extends Controller
      */
     public function show($id)
     {
-        $SStock = SStock::select('SK_NO', 'SK_NAME')->where('SK_NO', $id)->get();
-        return $SStock[0]['SK_NAME'];
+        $SStock = SStock::select(
+                                'SK_NO'
+                                , 'SK_NAME'
+                                , 'SK_SPEC'
+                                , 'SK_COLOR'
+                                , 'SK_SIZE'
+                                , 'SK_USE'
+                                , 'SK_LOCATE'
+                                , 'SK_SESPES'
+                                , 'SK_ESPES'
+                                , 'SK_REM'
+                                , 'SK_SMNETS'
+                            )->where('SK_NO', $id)->first();
+        return $SStock;
     }
 
     /**
@@ -70,7 +83,72 @@ class SStockController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $input = $request->input();
+        $SK_SPEC_tw = '';
+        $SK_SPEC_en = '';
+
+        foreach($input as $key => $value){
+            if($value){
+
+                // 中文規格
+                if(!strpos($key,'_en'))
+                {
+                    // 查詢中文規格名稱
+                    $SpecItemName = app('App\Http\Controllers\MenuSpecItemController')->getSpecItemName($key);
+                
+                    // 將中文規格每一行排成: 規格標題 全形空格 規格內容
+                    if($SpecItemName->count() > 0){
+                        $SpecItemName = $SpecItemName[0]['spec_item_name'];
+                        $SK_SPEC_tw .=  $SpecItemName."	".$value."\r\n";
+                    }
+                    
+                }
+                else if(strpos($key,'_en') && !str_starts_with($key,'SK_') && !str_starts_with($key,'name_for_sell'))
+                {
+                    // 將英文規格每一行排成: 規格標題 全形空格 規格內容
+                    $key = str_replace('_en','',$key);
+                    $key = str_replace('_',' ',$key);
+                    $key = str_replace('Max ','Max. ',$key);
+                    $SK_SPEC_en .= $key."	".$value."\r\n";
+                }
+
+            }
+
+        }
+
+        $SK_SPEC = rtrim($SK_SPEC_tw);
+        $SK_ESPES = rtrim($SK_SPEC_en);
+
+        if(!empty($input['zh-tw_description'].$input['zh-tw_features'].$input['en-us_description'].$input['en-us_features'])){
+            $SK_SMNETS = $input['zh-tw_description']
+                        .(($input['zh-tw_description'] == "")?"":"\r\n")."---Features---\r\n"
+                        .$input['zh-tw_features']
+                        .(($input['zh-tw_features'] == "")?"":"\r\n")."---DESCRIPTION---\r\n"
+                        .$input['en-us_description']
+                        .(($input['en-us_description'] == "")?"":"\r\n")."---Features---\r\n"
+                        .$input['en-us_features'];
+        }else{
+            $SK_SMNETS = '';
+        }
+
+        echo "寫入規格資料到".$id."現有臨時料號...<br>";
+        try {
+            $SStock = SStock::find($id)->update([
+                                                    'SK_SPEC' => $SK_SPEC,
+                                                    'SK_COLOR' => $input['Color'],
+                                                    'SK_USE' => $input['categories_text'],
+                                                    'SK_LOCATE' => $input['ProdType_text'],
+                                                    'SK_SESPES' => $input['name_for_sell_en'],
+                                                    'SK_ESPES' => $SK_ESPES,
+                                                    'SK_SMNETS' => $SK_SMNETS,
+                                                ]);
+
+            echo "寫入規格資料完成<br>";
+            
+        } catch (Throwable $e) {
+            print "Error: ".$e->getMessage();
+            return ;
+        }
     }
 
     /**
